723c86634f82a565684d1aed51be39126c7a1cd9
xxboomstick <ddchang1iz@gmail.com>
2023-12-29 16:39:04 -0500

    Candlestick

--- yfinancedata.py
+++ yfinancedata.py
@@ -1,16 +1,17 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime
+import plotly.graph_objects as go
 
-ticker = 'GOOGL'
-
-# Download historical data
-data = yf.download(ticker, period='1y', interval='1d')
-
-# Print the first few rows of the data
-print(data.head(5))
+ticker = 'TENB'
 
 info = yf.Ticker(ticker)
 print(list(filter(lambda s: not s.startswith("_"), dir(info))))
 print()
-for x in info.news:
-    print(x, end='\n')
+
+data = info.history(period='1y', interval='1d')
+
+
+fig = go.Figure(data=[go.Candlestick(x=data.index, open=data['Open'], close=data['Close'], high=data['High'], low=data['Low'])])
+
+fig.show()
