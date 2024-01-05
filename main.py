7a7f689a2d063ea4636fac21f0035dc4b311eee2
erthy8 <justin.chen5@yahoo.com>
2024-01-05 15:43:26 -0500

    Add discord token to bot

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import discord
+import openai
+from discord.ext import commands
+
+
+discord_token = "MTE5MjkyODY0OTgwNTA0NTk0Mw.GwEF8F.MccvgaldNMJxes3Qi9fHcxaukFLO5SL0Hg9XG4"
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='/', intents=intents)
+
+
+
+
+
 print("THIS IS UI")
