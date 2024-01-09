1f1505dff8a6b463594633b20322739efe8685af
erthy8 <justin.chen5@yahoo.com>
2024-01-08 20:36:29 -0500

    final discord initial setup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import discord
-import openai
 from discord.ext import commands
 
 
@@ -8,7 +7,33 @@ intents = discord.Intents.all()
 client = commands.Bot(command_prefix='/', intents=intents)
 
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 
+@client.listen()
+async def on_message(message):
+    if message.author == client.user:
+        return
+    msg = message.content.lower()
 
-print("THIS IS UI")
+    if msg == 'gnaij':
+        # await message.channel.send(' is fat')
+        await message.channel.send("https://media.discordapp.net/attachments/801962896761946132/1095392190424956959/aaaah.png?width=1338&height=1014")
+        await message.channel.send("https://tenor.com/view/sheesh-papa-kermit-drive-gif-12861924")
+
+
+    if msg == 'alan':
+        await message.channel.send(' is fat')
+
+    # if 'e' in msg:
+    #     await message.channel.send("SUB TO HUSHVALO")
+
+    if msg == 'i love bandmaker':
+        await message.channel.send('I love you too {}'.format(message.author.name))
+
+
+client.run(discord_token)
+
+
