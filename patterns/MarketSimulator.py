7fc7bae92bbe39b7b80d1f47a6d0b7b4b2e6389f
xxboomstick <ddchang1iz@gmail.com>
2024-02-03 19:59:22 -0500

    formatting

--- patterns/MarketSimulator.py
+++ patterns/MarketSimulator.py
@@ -17,11 +17,11 @@ def simulate_trading(data: pd.DataFrame, starting_cash: int = 1000, print_trades
     trades_made = 0
     
     if print_trades:
-        print(f"{'Date':<12} {'Buy/Sell':<10} {'Shares':<10} {'Price':<10} {'Value':<10}")
+        print(f"{'Date':<15} {'Buy/Sell':<10} {'Shares':<10} {'Price':<10} {'Value':<10}")
         
-    # Iterate up to the second to last day, sell all shares on the last day
+    # Iterate up to the most recent day, sell all shares on the last day
     # Buy/Sell at the opening price of the next day, simulates placing orders overnight
-    for day_idx in range(pattern_matcher.CANDLES_REQUIRED - 1, len(data) - 1):
+    for day_idx in range(pattern_matcher.CANDLES_REQUIRED - 1, len(data)):
         result = pattern_matcher.process(data=data.iloc[day_idx - Marubozu.CANDLES_REQUIRED + 1:day_idx + 1])  # Check if pattern can be detected from past day
         if result != []:  # pattern found
             current_day = result[0][0]
@@ -35,28 +35,29 @@ def simulate_trading(data: pd.DataFrame, starting_cash: int = 1000, print_trades
                 cost = round(shares_bought * next_day_price, 2)
                 shares += shares_bought
                 current_value -= cost
-                if print_trades:
-                    print(f"\033[92m{next_day_date:<12} {'Buy':<10} {shares_bought:<10} ${next_day_price:<10} ${cost:<10}\033[0m")
                 trades_made += 1
+                if print_trades:
+                    print(f"\033[92m{next_day_date:<15} {'Buy':<10} {shares_bought:<10} ${next_day_price:<10} ${cost}\033[0m")
             elif result[0][1] == -1:
                 # Sell the stock
                 shares_sold = shares  # Allows change in purchasing strategy
                 profit = round(shares_sold * next_day_price, 2)
                 shares -= shares_sold
                 current_value += profit
-                if print_trades:
-                    print(f"\033[91m{next_day_date:<12} {'Sell':<10} {shares_sold:<10} ${next_day_price:<10} ${profit:<10}\033[0m")
                 trades_made += 1
+                if print_trades:
+                    print(f"\033[91m{next_day_date:<15} {'Sell':<10} {shares_sold:<10} ${next_day_price:<10} ${profit}\033[0m")
                 
     # Sell all remaining stock on last day
     current_day = data.iloc[-1]
     current_day_price = round(current_day.Close, 2)
     formatted_date = format_date(current_day.name)
-    profit = round(shares * current_day_price, 2)
+    shares_sold = shares  # Allows change in purchasing strategy
+    profit = round(shares_sold * next_day_price, 2)
+    shares -= shares_sold
     current_value += profit
-    print(f"Sold {shares} shares(s) on {formatted_date} for ${profit} at {current_day_price} per share")
-    shares = 0
     trades_made += 1
+    print(f"\033[91m{formatted_date:<15} {'Sell':<10} {shares_sold:<10} ${current_day_price:<10} ${profit}\033[0m")
     
     # Calculating results
     total_profit = round(current_value - starting_cash, 2)
@@ -65,7 +66,7 @@ def simulate_trading(data: pd.DataFrame, starting_cash: int = 1000, print_trades
     return {'value': current_value, 'num_trades': trades_made, 'profit': total_profit, 'profit_percent': profit_percent}
 
 if __name__ == '__main__':
-    ticker = 'WYY'  # Choose ticker
+    ticker = 'ION'  # Choose ticker
     pattern_matcher: PatternMatcher = Marubozu()  # Choose pattern to be tested
     
     info = yf.Ticker(ticker)
